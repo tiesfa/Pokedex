8cba801502b24ead78c56d29517de7bce261072d
Ties <ties_aarts@outlook.com>
2023-09-17 22:39:04 +0200

    Icons in the pokemonlist

--- src/components/ListBlock.vue
+++ src/components/ListBlock.vue
@@ -1,39 +1,43 @@
 <template>
     <div class="text-white text-xs md:text-base bg-dark p-2 md:p-4 leading-loose h-36 md:h-[38rem] overflow-scroll">
         <ul>
-            <li>#001 - Bulbasaur</li>
-            <li>#001 - Bulbasaur</li>
-            <li>#001 - Bulbasaur</li>
-            <li>#001 - Bulbasaur</li>
-            <li>#001 - Bulbasaur</li>
-            <li>#001 - Bulbasaur</li>
-            <li>#001 - Bulbasaur</li>
-            <li>#001 - Bulbasaur</li>
-            <li>#001 - Bulbasaur</li>
-            <li>#001 - Bulbasaur</li>
-            <li>#001 - Bulbasaur</li>
-            <li>#001 - Bulbasaur</li>
-            <li>#001 - Bulbasaur</li>
-            <li>#001 - Bulbasaur</li>
-            <li>#001 - Bulbasaur</li>
-            <li>#001 - Bulbasaur</li>
-            <li>#001 - Bulbasaur</li>
-            <li>#001 - Bulbasaur</li>
-            <li>#001 - Bulbasaur</li>
-            <li>#001 - Bulbasaur</li>
-            <li>#001 - Bulbasaur</li>
-            <li>#001 - Bulbasaur</li>
-            <li>#001 - Bulbasaur</li>
-            <li>#001 - Bulbasaur</li>
-            <li>#001 - Bulbasaur</li>
-            <li>#001 - Bulbasaur</li>
-            <li>#001 - Bulbasaur</li>
-            <li>#001 - Bulbasaur</li>
-            <li>#001 - Bulbasaur</li>
-            <li>#001 - Bulbasaur</li>
-            <li>#001 - Bulbasaur</li>
-            <li>#001 - Bulbasaur</li>
-            <li>#001 - Bulbasaur</li>
+            <li>#001 - Bulbasaur - <img class="inline-block w-8" src="../assets/0001Bulbasaur.png" alt=""></li>
+            <li>#001 - Bulbasaur - <img class="inline-block w-8" src="../assets/0001Bulbasaur.png" alt=""></li>
+            <li>#001 - Bulbasaur - <img class="inline-block w-8" src="../assets/0001Bulbasaur.png" alt=""></li>
+            <li>#001 - Bulbasaur - <img class="inline-block w-8" src="../assets/0001Bulbasaur.png" alt=""></li>
+            <li>#001 - Bulbasaur - <img class="inline-block w-8" src="../assets/0001Bulbasaur.png" alt=""></li>
+            <li>#001 - Bulbasaur - <img class="inline-block w-8" src="../assets/0001Bulbasaur.png" alt=""></li>
+            <li>#001 - Bulbasaur - <img class="inline-block w-8" src="../assets/0001Bulbasaur.png" alt=""></li>
+            <li>#001 - Bulbasaur - <img class="inline-block w-8" src="../assets/0001Bulbasaur.png" alt=""></li>
+            <li>#001 - Bulbasaur - <img class="inline-block w-8" src="../assets/0001Bulbasaur.png" alt=""></li>
+            <li>#001 - Bulbasaur - <img class="inline-block w-8" src="../assets/0001Bulbasaur.png" alt=""></li>
+            <li>#001 - Bulbasaur - <img class="inline-block w-8" src="../assets/0001Bulbasaur.png" alt=""></li>
+            <li>#001 - Bulbasaur - <img class="inline-block w-8" src="../assets/0001Bulbasaur.png" alt=""></li>
+            <li>#001 - Bulbasaur - <img class="inline-block w-8" src="../assets/0001Bulbasaur.png" alt=""></li>
+            <li>#001 - Bulbasaur - <img class="inline-block w-8" src="../assets/0001Bulbasaur.png" alt=""></li>
+            <li>#001 - Bulbasaur - <img class="inline-block w-8" src="../assets/0001Bulbasaur.png" alt=""></li>
+            <li>#001 - Bulbasaur - <img class="inline-block w-8" src="../assets/0001Bulbasaur.png" alt=""></li>
+            <li>#001 - Bulbasaur - <img class="inline-block w-8" src="../assets/0001Bulbasaur.png" alt=""></li>
+            <li>#001 - Bulbasaur - <img class="inline-block w-8" src="../assets/0001Bulbasaur.png" alt=""></li>
+            <li>#001 - Bulbasaur - <img class="inline-block w-8" src="../assets/0001Bulbasaur.png" alt=""></li>
+            <li>#001 - Bulbasaur - <img class="inline-block w-8" src="../assets/0001Bulbasaur.png" alt=""></li>
+            <li>#001 - Bulbasaur - <img class="inline-block w-8" src="../assets/0001Bulbasaur.png" alt=""></li>
+            <li>#001 - Bulbasaur - <img class="inline-block w-8" src="../assets/0001Bulbasaur.png" alt=""></li>
+            <li>#001 - Bulbasaur - <img class="inline-block w-8" src="../assets/0001Bulbasaur.png" alt=""></li>
+            <li>#001 - Bulbasaur - <img class="inline-block w-8" src="../assets/0001Bulbasaur.png" alt=""></li>
+            <li>#001 - Bulbasaur - <img class="inline-block w-8" src="../assets/0001Bulbasaur.png" alt=""></li>
+            <li>#001 - Bulbasaur - <img class="inline-block w-8" src="../assets/0001Bulbasaur.png" alt=""></li>
+            <li>#001 - Bulbasaur - <img class="inline-block w-8" src="../assets/0001Bulbasaur.png" alt=""></li>
+            <li>#001 - Bulbasaur - <img class="inline-block w-8" src="../assets/0001Bulbasaur.png" alt=""></li>
+            <li>#001 - Bulbasaur - <img class="inline-block w-8" src="../assets/0001Bulbasaur.png" alt=""></li>
+            <li>#001 - Bulbasaur - <img class="inline-block w-8" src="../assets/0001Bulbasaur.png" alt=""></li>
+            <li>#001 - Bulbasaur - <img class="inline-block w-8" src="../assets/0001Bulbasaur.png" alt=""></li>
+            <li>#001 - Bulbasaur - <img class="inline-block w-8" src="../assets/0001Bulbasaur.png" alt=""></li>
+            <li>#001 - Bulbasaur - <img class="inline-block w-8" src="../assets/0001Bulbasaur.png" alt=""></li>
+            <li>#001 - Bulbasaur - <img class="inline-block w-8" src="../assets/0001Bulbasaur.png" alt=""></li>
+            <li>#001 - Bulbasaur - <img class="inline-block w-8" src="../assets/0001Bulbasaur.png" alt=""></li>
+            <li>#001 - Bulbasaur - <img class="inline-block w-8" src="../assets/0001Bulbasaur.png" alt=""></li>
+            <li>#001 - Bulbasaur - <img class="inline-block w-8" src="../assets/0001Bulbasaur.png" alt=""></li>
         </ul>
     </div>
 </template>
