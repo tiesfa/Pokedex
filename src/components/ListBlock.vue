302614bb118ba0543952c3341acfdb41379387e1
Ties <ties_aarts@outlook.com>
2023-09-18 19:50:18 +0200

    API Call vue 2 method

--- src/components/ListBlock.vue
+++ src/components/ListBlock.vue
@@ -1,20 +1,27 @@
 <template>
   <div class="text-white text-xs md:text-base bg-dark p-2 md:p-4 leading-loose h-36 md:h-[38rem] overflow-scroll">
     <ul>
-      <li v-for="(pokemon, i) in pokemonList" :key="`poke-${i}`" class="capitalize">#{{ pokemon.entry_number }} - {{
-        pokemon.pokemon_species.name }} - <img class="inline-block w-8" src="" alt="">
-      </li>
+      <!-- <li v-for="(pokemon, i) in pokemonList" :key="`poke-${i}`" class="capitalize">#{{ pokemon.entry_number }} - {{
+        pokemon.pokemon_species.name }} - <img class="inline-block w-8" :src="" alt="">
+      </li> -->
     </ul>
   </div>
 </template>
 
-<script setup>
-import { ref, computed, onMounted } from 'vue'
-
-const pokemonList = ref([])
-
-onMounted(async () => {
-  const pokeData = await fetch('https://pokeapi.co/api/v2/pokedex/2/').then(response => response.json())
-  pokemonList.value = pokeData.pokemon_entries
-})
+<script>
+  // Vue2 method
+  export default {
+    data() {
+      return {
+        response: null
+      }
+    },
+    async created() {
+      this.response = await fetch("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/150.png")
+      console.log(this.response);
+    },
+    mounted() {
+      
+    },
+  }
 </script>
