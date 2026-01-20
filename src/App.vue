90ba697c2126fe9aa77878b2c2e6d741103da0bb
Ties <19891773+tiesfa@users.noreply.github.com>
2026-01-20 20:12:17 +0100

    ui layout fix

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <main v-cloak class="container font-press-start">
+    <div class="text-xl md:text-3xl mt-4 lg:mt-8 text-light uppercase tracking-wider">
+      <h1>Pokedex</h1>
+    </div>
     <div class="grid md:grid-cols-2 md:grid-flow-col gap-4 lg:gap-8 py-4 md:py-8">
       <div class="md:col-span-1 flex flex-col gap-4 lg:gap-8">
-        <div class="text-xl md:text-3xl text-light uppercase tracking-wider">
-          <h1>Pokedex</h1>
-        </div>
         <DisplayBlock />
         <InfoBlock />
       </div>
