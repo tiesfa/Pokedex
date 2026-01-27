258733d3406c07200acac9950fb85157777e4ef8
Ties <19891773+tiesfa@users.noreply.github.com>
2026-01-28 00:50:28 +0100

    Fix 404 error Git pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/modern-pokedex/', // For GitHub Pages deployment
   plugins: [vue()],
 })
