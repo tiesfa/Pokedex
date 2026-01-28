06c2ec3c841ae7637810c3605b90225d4780b7de
Ties <19891773+tiesfa@users.noreply.github.com>
2026-01-28 01:10:49 +0100

    base name change

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: 'https://tiesfa.github.io/modern-pokedex/', // For GitHub Pages deployment
+  base: 'modern-pokedex', // For GitHub Pages deployment
   plugins: [vue()],
 })
