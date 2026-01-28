be614f99ef10043195365d2a3bc4b2b5752d91fd
Ties Aarts <19891773+tiesfa@users.noreply.github.com>
2026-01-28 11:01:22 +0100

    added a base for hostinger

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: 'modern-pokedex', // For GitHub Pages deployment
+  // base: 'modern-pokedex', // For GitHub Pages deployment
+  base: 'https://tiesfa.com/modern-pokedex/', // For Hostinger deployment
   plugins: [vue()],
 })
